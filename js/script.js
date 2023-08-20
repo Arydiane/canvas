9d0b76f67d33f03afddc737c77a1bd27590d1a22
Arydiane <arydianejardim@gmail.com>
2023-08-20 11:15:49 -0300

    desenha um triângulo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 const tela = document.querySelector('canvas'); 
 const pincel = tela.getContext('2d'); 
 
-// pincel.fillStyle = 'lightgray';
-// pincel.fillRect(0, 0, 600, 400);
+//desenha um retângulo
+pincel.fillStyle = 'lightgray';
+pincel.fillRect(0, 0, 600, 400);
 
 pincel.fillStyle = 'green'; 
 pincel.fillRect(0, 0, 200, 400); 
 
 pincel.fillStyle = 'red'; 
 pincel.fillRect(400, 0, 200, 400)
+
+//desenha um triângulo
+pincel.fillStyle = 'yellow'; 
+pincel.beginPath(); 
+pincel.moveTo(300, 200);
+pincel.lineTo(200, 400);
+pincel.lineTo(400, 400);
+pincel.fill();
+
+
