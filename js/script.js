c0d812f2953420914364a1abdbb917dc1d6cb81d
Arydiane <arydianejardim@gmail.com>
2023-08-20 11:05:26 -0300

    desenha a bandeira da itália

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const tela = document.querySelector('canvas'); 
 const pincel = tela.getContext('2d'); 
 
-pincel.fillStyle = "lightgray";
-pincel.fillRect(0, 0, 600, 400);
- 
+// pincel.fillStyle = 'lightgray';
+// pincel.fillRect(0, 0, 600, 400);
+
+pincel.fillStyle = 'green'; 
+pincel.fillRect(0, 0, 200, 400); 
+
+pincel.fillStyle = 'red'; 
+pincel.fillRect(400, 0, 200, 400)
