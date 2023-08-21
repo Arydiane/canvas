75d19a74c37e538eb48733f071034e6ef7851604
Arydiane <arydianejardim@gmail.com>
2023-08-21 12:35:36 -0300

    adiciona funcionalidade de alterar a cor do círculo ao clicar com botão direito altera a cor entre azul, vermelho e verde

--- js/programa2.js
+++ js/programa2.js
@@ -4,12 +4,15 @@ const pincel = tela.getContext('2d');
 pincel.fillStyle = 'gray'; 
 pincel.fillRect(0, 0, 600, 400); 
 
+const cores = ['blue', 'red', 'green']; 
+let indiceCorAtual = 0; 
+
 function desenhaCirculo(evento) {
     //posição clicada no documento - posição de deslocamento em relação ao elemento pai
     const x = evento.pageX - tela.offsetLeft; 
     const y = evento.pageY - tela.offsetTop;
 
-    pincel.fillStyle = 'blue'; 
+    pincel.fillStyle = cores[indiceCorAtual]; 
     pincel.beginPath(); 
     pincel.arc(x, y, 10, 0, 2 * 3.14); 
     pincel.fill(); 
@@ -18,3 +21,16 @@ function desenhaCirculo(evento) {
 }
 
 tela.onclick = desenhaCirculo; 
+
+function mudaCor() {
+   indiceCorAtual++; 
+   if (indiceCorAtual >= cores.length) {
+    indiceCorAtual = 0; 
+   }
+   alert(`Cor alterada para: ${cores[indiceCorAtual]}!`);
+    //retorna falso para que menu contextual padrão do canvas não seja exibido
+    return false; 
+}
+
+//identifica clique com botão direito do mouse
+tela.oncontextmenu = mudaCor; 
