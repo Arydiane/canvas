e518b4536a90c6b8fd1c571f4b253749a752637e
Arydiane <arydianejardim@gmail.com>
2023-08-21 18:32:50 -0300

    adiciona animação no sentido inverso

--- js/animacaoBolinha.js
+++ js/animacaoBolinha.js
@@ -10,17 +10,24 @@ function desenhaCirculo(x, y, raio) {
 }
 
 function limpaTela() {
-
     pincel.clearRect(0, 0, 600, 400);
 }
 
 let x = 20; 
+let sentido = 1; 
 
 function atualizaTela() {
-    
+
     limpaTela();
+
+    if (x > 600) {
+        sentido = -1; 
+    } else if (x < 0) {
+        sentido = 1; 
+    }
+
     desenhaCirculo(x, 20, 10); 
-    x++; 
+    x = x + sentido;  
 }
 
 setInterval(atualizaTela, 10);
